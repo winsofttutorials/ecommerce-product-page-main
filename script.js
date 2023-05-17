8ee933018f9f1963a8fe2298b79b4738502cbc7f
winsofttutorials <winsofttutorials@gmail.com>
2023-05-17 13:38:39 +0100

    sideleft

--- script.js
+++ script.js
@@ -7,3 +7,47 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.left = "-200px";
 }
+
+// --------------------------ACTIVE CLASS SCRITP----------------
+
+let activeProduct = document.getElementsByClassName("previewImg");
+let productImg = document.getElementById("productImg");
+let activeCovers = document.getElementsByClassName("coverImg");
+
+function activeBorder() {
+  for (const activePrdt of activeProduct) {
+    activePrdt.classList.remove("active");
+  }
+  event.currentTarget.classList.add("active");
+}
+
+function overLayering() {
+  for (const activeLayer of activeCovers) {
+    activeLayer.classList.remove("activeCover");
+  }
+  event.currentTarget.classList.add("activeCover");
+}
+
+activeProduct[0].onclick = function () {
+  productImg.src = "./images/image-product-1.jpg";
+  overLayering();
+  activeBorder();
+};
+
+activeProduct[1].onclick = function () {
+  productImg.src = "./images/image-product-2.jpg";
+  overLayering();
+  activeBorder();
+};
+
+activeProduct[2].onclick = function () {
+  productImg.src = "./images/image-product-3.jpg";
+  overLayering();
+  activeBorder();
+};
+
+activeProduct[3].onclick = function () {
+  productImg.src = "./images/image-product-4.jpg";
+  overLayering();
+  activeBorder();
+};
